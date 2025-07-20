5adbd3522b31f21e0e17b88e0dc3abbc33a8d5cc
samueljuma <samueljuma1024@gmail.com>
2025-07-20 07:30:46 +0300

    implement API endpoints for conversations and messages

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all().prefetch_related('participants', 'messages')
+    serializer_class = ConversationSerializer
+
+    def create(self, request, *args, **kwargs):
+        participant_ids = request.data.get('participants')
+
+        if not participant_ids or len(participant_ids) < 2:
+            return Response(
+                {"error": "At least two participants are required to start a conversation."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        conversation = Conversation.objects.create()
+        conversation.participants.set(participant_ids)
+        serializer = self.get_serializer(conversation)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    serializer_class = MessageSerializer
+
+    def get_queryset(self):
+        
+        conversation_id = self.request.query_params.get('conversation_id')
+        qs = Message.objects.all().select_related('sender', 'conversation')
+
+        if conversation_id:
+            qs = qs.filter(conversation__conversation_id=conversation_id)
+
+        return qs.order_by('sent_at')
+
+    def create(self, request, *args, **kwargs):
+        conversation_id = request.data.get('conversation')
+        sender_id = request.data.get('sender')
+
+        conversation = get_object_or_404(Conversation, conversation_id=conversation_id)
+
+        message = Message.objects.create(
+            conversation=conversation,
+            sender_id=sender_id,
+            message_body=request.data.get('message_body')
+        )
+
+        serializer = self.get_serializer(message)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
